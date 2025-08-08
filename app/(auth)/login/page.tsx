86dee993468e405549ccbd97e21ae5c2f7a20acf
Jana Marie Bantolino <bantolino.jana@gmail.com>
2025-08-09 00:41:56 +0800

    fix: update layout and styling for authentication page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,7 +9,7 @@ export default function AuthenticationPage() {
   return (
     <>
       {/* Desktop grid */}
-      <div className="relative container hidden flex-1 shrink-0 items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative grid w-full min-h-[100svh] items-stretch grid-rows-[auto,1fr] lg:grid-rows-1 lg:grid-cols-2 lg:px-0">
         <Link
           href="/login"
           className={cn(
@@ -21,34 +21,16 @@ export default function AuthenticationPage() {
         </Link>
 
         {/* Left panel */}
-        <div className="text-primary relative hidden h-full flex-col p-10 lg:flex dark:border-r">
+        <div className="text-primary relative flex h-full flex-col p-8 lg:p-10 border-b lg:border-b-0 lg:border-r">
           <div className="bg-primary/5 absolute inset-0" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="leading-normal text-balance">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo; - Sofia Davis
-            </blockquote>
+            {/* TODO: Add logo */}
+            BetterInternship
           </div>
         </div>
 
         {/* Right panel (form) */}
-        <div className="flex items-center justify-center lg:h-[1000px] lg:p-8">
+        <div className="flex items-center justify-center h-full p-6 lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center gap-6 sm:w-[350px]">
             <div className="flex flex-col gap-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
