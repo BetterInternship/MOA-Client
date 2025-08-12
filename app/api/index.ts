21c4ca4d42db0dea90fa8a4015e7b4d972a0462e
neue-dev <modavid.1964@gmail.com>
2025-08-13 01:17:57 +0800

    update endpoints based on server impl

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,3 +1,5 @@
 export * from "./app/api/endpoints/app/app";
 export * from "./app/api/endpoints/auth/auth";
 export * from "./app/api/models";
+export * from "./app/api/endpoints/school-moa/school-moa";
+export * from "./app/api/endpoints/school-entities/school-entities";
