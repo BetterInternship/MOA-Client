74fc9fc6c5e225d6a6f98b58ba40ad28037f7be6
neue-dev <modavid.1964@gmail.com>
2025-08-13 01:24:19 +0800

    Create endpoints for univ side yippee!

---
+++ app/api/school.api.tsx
@@ -0,0 +1,39 @@
+import {
+  useSchoolEntitiesControllerApproveRequest,
+  useSchoolEntitiesControllerDenyRequest,
+  useSchoolEntitiesControllerGetMyPartners,
+  useSchoolEntitiesControllerGetMyRequests,
+} from "./app/api/endpoints/school-entities/school-entities";
+import {
+  useSchoolMoaControllerApprove,
+  useSchoolMoaControllerDeny,
+  useSchoolMoaControllerGetMine,
+} from "./app/api/endpoints/school-moa/school-moa";
+
+export const useEntities = () => {
+  const entities = useSchoolEntitiesControllerGetMyPartners();
+};
+
+export const useMoaRequests = () => {
+  const requests = useSchoolMoaControllerGetMine();
+  const approve = useSchoolMoaControllerApprove();
+  const deny = useSchoolMoaControllerDeny();
+
+  return {
+    requests: requests.data,
+    approve: approve.mutateAsync,
+    deny: deny.mutateAsync,
+  };
+};
+
+export const useNewEntityRequests = () => {
+  const requests = useSchoolEntitiesControllerGetMyRequests();
+  const approve = useSchoolEntitiesControllerApproveRequest();
+  const deny = useSchoolEntitiesControllerDenyRequest();
+
+  return {
+    requests: requests.data,
+    approve: approve.mutateAsync,
+    deny: deny.mutateAsync,
+  };
+};
