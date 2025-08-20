d369694451632b542dabfdc5c10b50f084b8aa25
neue-dev <modavid.1964@gmail.com>
2025-08-21 01:33:28 +0800

    fix up company landing

--- app/moa/(auth)/login/page.tsx
+++ app/moa/(auth)/login/page.tsx
@@ -6,18 +6,20 @@ export default function CompanyAuthPage() {
     <div className="relative grid min-h-[100svh] w-full items-stretch lg:grid-cols-2 lg:px-0">
       {/* Left panel (form) */}
       <div className="flex h-full items-center justify-center p-6 lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center gap-6 sm:w-[400px]">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Company Login</h1>
+        <div className="mx-auto flex w-full flex-col gap-1 sm:w-[400px]">
+          <div className="flex flex-col gap-4 text-left">
+            <h1 className="text-3xl font-bold tracking-tight">Company Login</h1>
             <p className="text-muted-foreground text-sm">
               Start or manage your Memorandum of Agreement with <br />
-              <span className="text-foreground font-medium">De La Salle University</span>
+              <span className="text-muted-foreground font-medium">De La Salle University</span>
             </p>
           </div>
+          <br />
 
           <CompanyAuthForm />
+          <br />
 
-          <p className="text-muted-foreground px-8 text-center text-xs leading-relaxed">
+          <p className="text-muted-foreground mt-8 text-left text-xs leading-relaxed">
             By continuing, you agree to the{" "}
             <Link href="/terms" className="hover:text-primary underline underline-offset-4">
               Terms of Service
@@ -29,7 +31,7 @@ export default function CompanyAuthPage() {
             .
           </p>
 
-          <p className="text-center text-sm">
+          <p className="text-left text-xs">
             Don't have an account?{" "}
             <Link href="/register" className="hover:text-primary underline">
               Register your company
@@ -42,11 +44,11 @@ export default function CompanyAuthPage() {
       <div
         className="relative hidden flex-col items-end border-l bg-cover bg-center p-10 text-right lg:flex"
         style={{
-          backgroundImage: "url('moa-login.jpg')",
+          backgroundImage: "url('backgrounds/moa-login.jpg')",
         }}
       >
         {/* Overlay */}
-        <div className="bg-primary/60 absolute inset-0" />
+        <div className="bg-primary/40 absolute inset-0" />
 
         <div className="relative z-20 flex items-center gap-2 text-lg font-semibold text-white">
           <img src="/betterinternship-logo.png" alt="Logo" width={28} height={28} />
