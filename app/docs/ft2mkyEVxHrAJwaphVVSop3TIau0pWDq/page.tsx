464070fe5dab30c33bde09b1ac423b4e2d81bc3c
neue-dev <modavid.1964@gmail.com>
2025-10-26 16:35:56 +0800

    feat: field preview + make it easier to edit fields

--- app/docs/ft2mkyEVxHrAJwaphVVSop3TIau0pWDq/page.tsx
+++ app/docs/ft2mkyEVxHrAJwaphVVSop3TIau0pWDq/page.tsx
@@ -1,7 +1,7 @@
 /**
  * @ Author: BetterInternship
  * @ Create Time: 2025-10-25 04:12:44
- * @ Modified time: 2025-10-26 14:00:06
+ * @ Modified time: 2025-10-26 16:35:05
  * @ Description:
  *
  * This page will let us upload forms and define their schemas on the fly.
@@ -10,7 +10,7 @@
 "use client";
 
 import { Loader } from "@/components/ui/loader";
-import { useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import {
   AreaHighlight,
   Comment,
@@ -28,7 +28,9 @@ import { CheckCircle, PlusCircle, Upload } from "lucide-react";
 import { IFormField } from "@betterinternship/core";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/app/providers/modal-provider";
+import { createPortal } from "react-dom";
 import path from "path";
+import { cn } from "@/lib/utils";
 
 /**
  * Helps us upload forms and find their coords quickly.
@@ -71,13 +73,24 @@ const FormUploadPage = () => {
 
     // Maybe in the future you want to return a component to render per highlight
     // Do it here
-    return <></>;
+    return null;
   };
 
   // Adds a new field to be displayed
   const addField = (field: IFormField) => {
+    setHighlight(null);
     setFields([...fields, field]);
   };
+
+  // Edit field from the array
+  // This breaks the rules on how to use useState but makes up for it by calling setState() on the updated array LMAO
+  const editField = useCallback(
+    (key: number) => (newField: Partial<IFormField>) => {
+      fields[key] = { ...fields[key], ...newField };
+      setFields(fields.slice());
+    },
+    [fields]
+  );
 
   // Renders a highlight into a component
   const highlightRenderer = (highlight: ViewportHighlight, index: number) => {
@@ -97,6 +110,7 @@ const FormUploadPage = () => {
           setDocumentUrl={setDocumentUrl}
           documentFields={fields}
           addDocumentField={addField}
+          editDocumentField={editField}
         />
         {documentUrl && (
           <PdfLoader url={documentUrl} beforeLoad={<Loader />}>
@@ -128,29 +142,45 @@ const FieldEditor = ({
   initialX,
   initialY,
   initialW,
+  initialPage,
   fieldName,
+  selected,
+  updateField,
 }: {
   initialX: number;
   initialY: number;
   initialW: number;
+  initialPage: number;
   fieldName: string;
+  selected: boolean;
+  updateField: (field: { field: string; x: number; y: number; w: number; page: number }) => void;
 }) => {
   const [x, setX] = useState(initialX);
   const [y, setY] = useState(initialY);
   const [w, setW] = useState(initialW);
+  const [page, setPage] = useState(initialPage);
   const [field, setField] = useState(fieldName);
 
+  useEffect(() => {
+    updateField({ field, x, y, w, page });
+  }, [field, x, y, w, page]);
+
   return (
-    <div className="flex flex-col gap-2 rounded-[0.33em] border border-gray-300 p-2">
+    <div
+      className={cn(
+        "flex flex-col gap-2 rounded-[0.25em] border p-2",
+        selected ? "border-supportive bg-supportive/10" : "border-gray-300"
+      )}
+    >
       <div className="flex flex-row gap-2">
         <Input
           className="py-1"
           placeholder={"enter-field-name"}
           defaultValue={field}
-          onChange={(e) => setField(field)}
+          onChange={(e) => setField(e.target.value)}
         ></Input>
       </div>
-      <div className="flex flex-row gap-2">
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
         x:
         <Input
           type="number"
@@ -172,7 +202,61 @@ const FieldEditor = ({
           defaultValue={w}
           onChange={(e) => setW(parseInt(e.target.value))}
         ></Input>
+        page:
+        <Input
+          type="number"
+          className="py-1"
+          defaultValue={page}
+          onChange={(e) => setPage(parseInt(e.target.value))}
+        ></Input>
       </div>
+    </div>
+  );
+};
+
+/**
+ * A preview of what the field will look like on the document.
+ *
+ * @component
+ */
+const FieldPreview = ({
+  field,
+  x,
+  y,
+  w,
+  h,
+  selected,
+  onClick,
+}: {
+  field: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  selected: boolean;
+  onClick: () => void;
+}) => {
+  return (
+    <div
+      className={cn(
+        "absolute top-0 left-0 border-0!",
+        selected ? "bg-supportive/50!" : "bg-warning/50!"
+      )}
+      onClick={() => (onClick(), console.log("clicked field"))}
+      onMouseDown={onClick}
+      style={{
+        userSelect: "auto",
+        display: "inline-block",
+        width: `${w}px`,
+        height: `${h}px`,
+        fontSize: "12px",
+        transform: `translate(${x}px, ${y}px)`,
+        boxSizing: "border-box",
+        cursor: "pointer",
+        flexShrink: "0",
+      }}
+    >
+      {field}
     </div>
   );
 };
@@ -188,17 +272,21 @@ const Sidebar = ({
   setDocumentUrl,
   documentFields,
   addDocumentField,
+  editDocumentField,
 }: {
   fieldTransform: { x: number; y: number; w: number; h: number; page: number };
   setDocumentUrl: (documentUrl: string) => void;
   documentFields: IFormField[];
   addDocumentField: (field: IFormField) => void;
+  editDocumentField: (key: number) => (field: Partial<IFormField>) => void;
 }) => {
   // Allows us to click the input without showing it
   const fileInputRef = useRef<HTMLInputElement>(null);
   const { openModal, closeModal } = useModal();
   const [documentName, setDocumentName] = useState<string>("No File Selected");
   const [documentFile, setDocumentFile] = useState<File | null>(null);
+  const [fieldPreviews, setFieldPreviews] = useState<React.ReactNode[]>([]);
+  const [selectedFieldKey, setSelectedFieldKey] = useState<number | null>(null);
 
   // Handle changes in file upload
   const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -211,16 +299,17 @@ const Sidebar = ({
   };
 
   // Handle when a field is added by user
-  const handleFieldAdd = () => {
+  const handleFieldAdd = useCallback(() => {
     addDocumentField({
       ...fieldTransform,
+      h: 12,
       field: "enter-field-name",
       type: "text",
       validator: "",
       transformer: "",
       prefiller: "",
     });
-  };
+  }, [fieldTransform]);
 
   // Handles when a file is registered to the db
   const handleFileRegister = () => {
@@ -231,13 +320,55 @@ const Sidebar = ({
     );
   };
 
+  // Makes sure that the selected field is always shown at the top
+  const sortedDocumentFields = useMemo(() => {
+    if (selectedFieldKey === null) return documentFields.toReversed();
+    console.log("ind", selectedFieldKey);
+    return [
+      documentFields[selectedFieldKey],
+      ...documentFields.filter((f, i) => i !== selectedFieldKey).toReversed(),
+    ];
+  }, [selectedFieldKey, documentFields]);
+
+  // Updates the field previews
+  // (we have to touch the DOM directly for this to go under the hoods of the lib we're using)
+  useEffect(() => {
+    const fieldPreviews = [];
+    const fieldPreviewContainers =
+      document.querySelectorAll(".PdfHighlighter__highlight-layer") ?? [];
+
+    for (let i = 0; i < documentFields.length; i++) {
+      const field = documentFields[i];
+      const fieldPreviewContainer = fieldPreviewContainers[field.page - 1];
+      if (!fieldPreviewContainer) continue;
+      fieldPreviews.push(
+        createPortal(
+          <FieldPreview
+            field={field.field}
+            x={field.x}
+            y={field.y}
+            w={field.w}
+            h={field.h}
+            selected={i === selectedFieldKey}
+            onClick={() => setSelectedFieldKey(i)}
+          />,
+          fieldPreviewContainer
+        )
+      );
+    }
+
+    setFieldPreviews(fieldPreviews);
+    return () => setFieldPreviews([]);
+  }, [selectedFieldKey, documentFields]);
+
   return (
     <div className="sidebar w-[25vw] p-10">
       <h1 className="my-2 text-lg font-bold">{documentName}</h1>
       <pre className="my-2">
-        x: {fieldTransform.x}, y: {fieldTransform.y}, w: {fieldTransform.w}, h: {fieldTransform.h}
+        x: {fieldTransform.x}, y: {fieldTransform.y}, w: {fieldTransform.w}, h: {fieldTransform.h},
+        page: {fieldTransform.page}
       </pre>
-      <div className="flex flex-row gap-2">
+      <div className="mb-2 flex flex-row gap-2">
         {documentFile && (
           <Button variant="outline" onClick={handleFieldAdd}>
             <PlusCircle />
@@ -250,10 +381,12 @@ const Sidebar = ({
             Register File
           </Button>
         )}
-        <Button onClick={() => fileInputRef.current?.click()}>
-          <Upload />
-          Select File
-        </Button>
+        {!documentFile && (
+          <Button onClick={() => fileInputRef.current?.click()}>
+            <Upload />
+            Select File
+          </Button>
+        )}
       </div>
       <input
         ref={fileInputRef}
@@ -262,17 +395,21 @@ const Sidebar = ({
         className="hidden"
         onChange={handleFileSelect}
       />
-      <hr className="my-2" />
-      <div className="flex flex-col">
-        {documentFields.map((field) => (
+      <div className="flex flex-col gap-2">
+        {sortedDocumentFields.map((field) => (
           <FieldEditor
+            key={documentFields.indexOf(field)}
             initialX={field.x}
             initialY={field.y}
             initialW={field.w}
+            initialPage={field.page}
             fieldName={field.field}
-          ></FieldEditor>
+            selected={documentFields.indexOf(field) === selectedFieldKey}
+            updateField={editDocumentField(documentFields.indexOf(field))}
+          />
         ))}
       </div>
+      {fieldPreviews}
     </div>
   );
 };
