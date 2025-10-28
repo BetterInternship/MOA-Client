79b682d10e147fcf32ce9aaffca26877f4812d26
neue-dev <modavid.1964@gmail.com>
2025-10-29 01:55:24 +0800

    add field types and source to form editor

--- app/docs/ft2mkyEVxHrAJwaphVVSop3TIau0pWDq/editor/page.tsx
+++ app/docs/ft2mkyEVxHrAJwaphVVSop3TIau0pWDq/editor/page.tsx
@@ -1,7 +1,7 @@
 /**
  * @ Author: BetterInternship
  * @ Create Time: 2025-10-25 04:12:44
- * @ Modified time: 2025-10-29 00:55:10
+ * @ Modified time: 2025-10-29 01:55:14
  * @ Description:
  *
  * This page will let us upload forms and define their schemas on the fly.
@@ -306,8 +306,20 @@ const FieldEditor = ({
           defaultValue={fieldDetails.label}
           onChange={handleChangeFactory("label")}
         />
+        <Badge>Type</Badge>
+        <Autocomplete
+          value={fieldDetails.type}
+          inputClassName="h-7 py-1 text-xs"
+          placeholder="Select Field Type"
+          options={[
+            { id: "text", name: "text" },
+            { id: "signature", name: "signature" },
+          ]}
+          setter={(id) => id && handleChangeFactory("type")(id)}
+        />
         <Badge>Source</Badge>
         <Autocomplete
+          value={fieldDetails.source}
           inputClassName="h-7 py-1 text-xs"
           placeholder="Select Field Source"
           options={[
