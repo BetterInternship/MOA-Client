95c0d1e1848bbaf304cb0e33fe60e680ab69920d
Jana Marie Bantolino <bantolino.jana@gmail.com>
2025-11-30 18:26:34 +0800

    fix: refactor header section to use Link component for logo navigation

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Header from "@/components/docs/Header";
 import { Providers } from "./providers";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Document Verification | BetterInternship",
@@ -13,16 +14,23 @@ export default function DocsLayout({ children }: { children: React.ReactNode }) 
       <body className="flex h-[100svh]! w-[100vw] flex-col justify-evenly overflow-x-hidden bg-white">
         <header className="bg-background/70 h-[4em] border-b backdrop-blur">
           <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-2">
-            <div className="flex items-center gap-2">
-              <img
-                src="/betterinternship-logo.png"
-                alt="Logo"
-                width={28}
-                height={28}
-                className="flex-none"
-              />
-              <div className="flex items-center gap-2 font-semibold">BetterInternship</div>
-            </div>
+            <Link
+              href={"/"}
+              className="block border-none text-black! outline-none focus:outline-none"
+            >
+              <div className="flex items-center gap-2">
+                <img
+                  src="/betterinternship-logo.png"
+                  alt="Logo"
+                  width={30}
+                  height={30}
+                  className="flex-none"
+                />
+                <h1 className="mb-0.5 flex items-center gap-2 font-semibold tracking-tighter sm:text-2xl">
+                  BetterInternship
+                </h1>
+              </div>
+            </Link>
             <div className="flex flex-1 justify-end gap-2">
               <Header />
             </div>
