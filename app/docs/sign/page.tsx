a9be2bc4f6f0819d7849fcc20dc0ad5b4404cab6
neue-dev <modavid.1964@gmail.com>
2025-12-08 14:13:27 +0800

    fix: make sure pdf viewer doesn't shrink if form is small

--- app/docs/sign/page.tsx
+++ app/docs/sign/page.tsx
@@ -367,7 +367,7 @@ function PageContent() {
   }, [formName, formVersion, formRes]);
 
   return (
-    <div className="relative mx-auto flex max-h-[100%] flex-col items-center space-y-4 overflow-y-hidden px-4 py-8">
+    <div className="relative mx-auto flex h-[100%] max-h-[100%] flex-col items-center space-y-4 overflow-y-hidden px-4 py-8">
       <div className="w-full max-w-7xl overflow-x-visible overflow-y-visible sm:w-7xl">
         <h2 className="text-justify text-sm tracking-tight whitespace-normal sm:text-base sm:whitespace-nowrap">
           Internship Document Fill-out Request from{" "}
@@ -377,7 +377,7 @@ function PageContent() {
           {pendingInfo?.pendingInfo?.form_label as string}
         </h1>
       </div>
-      <div className="relative flex w-full max-w-7xl flex-col justify-center gap-7 overflow-y-hidden sm:w-7xl sm:flex-row">
+      <div className="relative flex h-[100%] w-full max-w-7xl flex-col justify-center gap-7 overflow-y-hidden sm:w-7xl sm:flex-row">
         <div className="relative max-h-[100%] overflow-y-auto">
           {/* Form Renderer */}
           <div className="h-full max-h-[100%] space-y-4 overflow-y-auto rounded-[0.33em] border border-gray-300 p-5">
