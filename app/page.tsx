ef4ccd56d80fa6365d72647347e74447ca79404a
neue-dev <modavid.1964@gmail.com>
2025-12-10 14:02:43 +0800

    fix: show only manual fields for student previews

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@betterinternship/package.components";
 
 export default function HomePage() {
   function redirectToSubdomain(subdomain: string) {
