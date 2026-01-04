495f514aedbad957a95c6e8b44b30cf3f28c4695
Jana Marie Bantolino <bantolino.jana@gmail.com>
2026-01-05 03:35:14 +0800

    hmm?

--- components/docs/form-editor/form-layout/FormPreview.tsx
+++ components/docs/form-editor/form-layout/FormPreview.tsx
@@ -366,21 +366,3 @@ export const FormPreview = ({
     />
   );
 };
-
-export const FormPreview = ({
-  formName,
-  blocks,
-  signingParties,
-  documentUrl,
-  metadata,
-}: FormPreviewProps) => {
-  return (
-    <FormPreviewContent
-      formName={formName}
-      blocks={blocks}
-      signingParties={signingParties}
-      documentUrl={documentUrl}
-      metadata={metadata}
-    />
-  );
-};
