99a8f52e19b21ad041a4e3e4d0091dd99fc16c1d
neue-dev <modavid.1964@gmail.com>
2026-03-06 18:25:01 +0800

    chore: long-awaited font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  theme: { extend: {} },
+  theme: { 
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+        ],
+        heading: [
+          "Inter",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+        ],
+      }
+    } 
+  },
   plugins: [require("tailwindcss-animate")],
 };
